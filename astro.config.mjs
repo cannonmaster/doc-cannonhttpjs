abad2167f111171ac0e2a79547097754f4cf3526
cannon <cannonmaster666@gmail.com>
2023-07-10 20:41:49 -0700

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,44 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'My Docs',
+      title: "CannonHttpJS",
       social: {
-        github: 'https://github.com/withastro/starlight',
+        github: "https://github.com/cannonmaster/CannonHttpJS",
       },
       sidebar: [
         {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
-          ],
+          label: "The basics",
+          translations: { "zh-CN": "基本" },
+          autogenerate: { directory: "basic" },
+          // items: [
+          //   // Each item here is one entry in the navigation menu.
+          //   { label: "Example Guide", link: "/guides/example/" },
+          // ],
         },
         {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
+          label: "Advanced",
+          translations: { "zh-CN": "高级使用" },
+          autogenerate: { directory: "advanced" },
         },
       ],
+      defaultLocale: "en",
+      locales: {
+        en: {
+          label: "English",
+          lang: "en",
+        },
+        zh: {
+          label: "简体中文",
+          lang: "zh-CN",
+        },
+      },
     }),
   ],
 
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+  image: { service: { entrypoint: "astro/assets/services/sharp" } },
 });
